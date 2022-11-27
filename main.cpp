acfe5f6c4fa788b1da2e45666daa2e9547198e92
edwinspies <edwin.spies@live.de>
2022-11-27 01:40:20 +0100

    add starting parameters for filename and filetype

--- main.cpp
+++ main.cpp
@@ -5,16 +5,21 @@
 #include <algorithm>
 #include <tuple>
 #include <chrono>
+#include <getopt.h>
+#include <cstring>
 
 using namespace std;
 
-int main() {
+void operations(const string &filename, bool fileformat) {
+  DataParser *dp;
   set<string> allBoardIDs;
-  list<tuple<int, string, string>> samplesOfUniqueBoardID;
+  list<tuple<int, string, string, string>> samplesOfUniqueBoardID;
 
   auto start = std::chrono::steady_clock::now();
 
-  auto *dp = new DataParser("sram-result.csv");
+  dp = new DataParser(filename, fileformat);
+
+
   //output a file for each sample in the respective folder
   //Use this function to generate the data for the python script
   //dp->processAndOutputDataToNDFormat();
@@ -22,16 +27,52 @@ int main() {
   //These lines of code will generate a picture for the first board ID that is in the list
   allBoardIDs = dp->extractAllBoardIDs();
 
-  for (const string& s: allBoardIDs) {
+  int i = 0;
+  for (const string &s : allBoardIDs) {
+	if (i > 0) break;
 	samplesOfUniqueBoardID = dp->extractSamplesByBoardID(s);
 	DataParser::outputGraph(samplesOfUniqueBoardID);
+	i++;
   }
 
   auto end = std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsed_seconds = end-start;
+  std::chrono::duration<double> elapsed_seconds = end - start;
   std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
-  delete(dp);
+  delete (dp);
+}
+
+int main(int argc, char **argv) {
+  int opt;
+  string filename;
+  bool altFileFormat = false;
+
+  if (argc < 2) {
+	cout << "Wrong input parameter, try again" << endl;
+	cout << "Usage: " << argv[0] << "-f {CSV filename} -i {sergio or request}" << endl;
+  }
+
+  while ((opt = getopt(argc, argv, "f:i:")) != -1) {
+	switch (opt) {
+	  case 'f': filename = optarg;
+		break;
+	  case 'i':
+		if (strcmp(optarg, "alternative") == 0) {
+		  altFileFormat = true;
+		  break;
+		}
+		if (strcmp(optarg, "request") == 0) {
+		  altFileFormat = false;
+		  break;
+		}
+	  default: /* '?' */
+		fprintf(stderr, "Usage: %s -f {CSV filename} -i {request or alternative}\n", argv[0]);
+		exit(-1);
+	}
+  }
+
+  operations(filename, altFileFormat);
+
   return 0;
 }
 
